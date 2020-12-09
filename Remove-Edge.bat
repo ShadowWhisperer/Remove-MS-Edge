c66b0190aef4bf81caf384ce3bc9d3a29792616e
Sean H <61057625+ShadowWhisperer@users.noreply.github.com>
2020-12-09 09:03:52 -0600

    Added Script

---
+++ Remove-Edge.bat
@@ -0,0 +1,61 @@
+@echo off
+
+::
+:: Checks if Microsoft Edge(Chromium Based)is installed, and uninstall it
+:: Only Works with the newer Edge built on Chromium
+::
+:: Creator: ShadowWhisperer
+::  Github: https://github.com/ShadowWhisperer
+:: Updated: 12/09/2020
+::
+
+:: Check if ran as Admin
+net session >nul 2>&1
+if %errorLevel% == 0 (
+ cls
+) else (
+ echo. & echo Run Script As Admin & echo.
+ pause & exit
+)
+
+:: Set Exist Variable - Check if Edge is intalled  *Show Message, if not installed
+set "EXIST=0"
+:: Stop Edge Task
+taskkill /im "msedge.exe" /f  >nul 2>&1
+
+:: Uninstall - 32Bit
+if exist "C:\Program Files (x86)\Microsoft\Edge\Application\" (
+set "EXIST=1"
+for /f "delims=" %%a in ('dir /b "C:\Program Files (x86)\Microsoft\Edge\Application\"') do (
+cd /d "C:\Program Files (x86)\Microsoft\Edge\Application\%%a\Installer\"
+if exist "setup.exe" (
+echo - Removing Microsoft Edge
+start /w setup.exe --uninstall --system-level --force-uninstall)
+takeown /F "C:\Program Files (x86)\Microsoft\Edge\" /A /R /D y >nul 2>&1
+icacls "C:\Program Files (x86)\Microsoft\Edge\" /grant administrators:F /T >nul 2>&1
+rmdir /s/q "C:\Program Files (x86)\Microsoft\Edge\" >nul 2>&1
+echo Finished
+timeout /t 3 & exit
+))
+
+:: Uninstall - 64Bit
+if exist "C:\Program Files\Microsoft\Edge\Application\" (
+set "EXIST=1"
+for /f "delims=" %%a in ('dir /b "C:\Program Files\Microsoft\Edge\Application\"') do (
+cd /d "C:\Program Files\Microsoft\Edge\Application\%%a\Installer\"
+if exist "setup.exe" (
+echo - Removing Microsoft Edge
+start /w setup.exe --uninstall --system-level --force-uninstall)
+takeown /F "C:\Program Files\Microsoft\Edge\" /A /R /D y >nul 2>&1
+icacls "C:\Program Files\Microsoft\Edge\" /grant administrators:F /T >nul 2>&1
+rmdir /s/q "C:\Program Files\Microsoft\Edge\" >nul 2>&1
+echo Finished
+timeout /t 3 & exit
+))
+
+
+:: Not Installed
+if "%EXIST%"=="0" (
+echo. & echo Edge ^(Chromium^) Is Not Installed & echo.
+timeout /t 3 & exit)
+
